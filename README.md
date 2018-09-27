4fcaee61eb9e5a1c7f9c824e0e3d80eddc6fe0c3
RomanticWd <402468285@qq.com>
2018-09-26 23:46:20 -0700

    First init by Wd

---
+++ README.md
@@ -0,0 +1,27 @@
+# webpack_vue_demo
+
+> Created by Vue-cli, Modified it on this basis, You can run it on your own computer.
+You should execute the following commands depending on your situation.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run all tests
+npm test
+```
+
---
+++ src/common/head.vue
@@ -0,0 +1,34 @@
+<template>
+<el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+  <el-menu-item index="1">首页</el-menu-item>
+  <el-submenu index="2">
+    <template slot="title">我的工作台1</template>
+    <el-menu-item index="2-1">选项1</el-menu-item>
+    <el-menu-item index="2-2">选项2</el-menu-item>
+    <el-menu-item index="2-3">选项3</el-menu-item>
+    <el-submenu index="2-4">
+      <template slot="title">产品</template>
+      <el-menu-item index="2-4-1">产品1</el-menu-item>
+      <el-menu-item index="2-4-2">产品2</el-menu-item>
+      <el-menu-item index="2-4-3">产品3</el-menu-item>
+    </el-submenu>
+  </el-submenu>
+  <el-menu-item index="3" disabled>消息中心</el-menu-item>
+  <el-menu-item index="4"><a href="https://www.pornhub.com" target="_blank">用户管理</a></el-menu-item>
+</el-menu>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: "1"
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+};
+</script>
