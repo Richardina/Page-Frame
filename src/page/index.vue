582d86627fe38c75b33f54cbcdfbee493b2b70e7
OldBeeUnderBlade <39401504@qq.com>
2018-09-28 10:24:00 -0400

    add popup

---
+++ src/page/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <y-head></y-head>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import YHead from '../common/head'
+export default {
+  name: 'App',
+  components: {
+    YHead
+  }
+}
+</script>
+
+<style>
+</style>
